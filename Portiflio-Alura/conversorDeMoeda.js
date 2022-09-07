335e10a65ec1597f3602c3832a870fcd3e01a427
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-09-07 18:39:30 -0300

    edit

--- Portiflio-Alura/conversorDeMoeda.js
+++ Portiflio-Alura/conversorDeMoeda.js
@@ -1,5 +1,7 @@
-function ConverterDolar() {
-    var valorElemento = document.getElementById("valor")
+
+
+function ConverterReal() {
+    var valorElemento = document.getElementById("valor1")
     var valor = valorElemento.value
     var valorNumber = parseFloat(valor)
   
@@ -8,7 +10,7 @@ function ConverterDolar() {
 
     
   
-    var valorElemento = document.getElementById("valorConvertidoReal")
+    var valorElemento = document.getElementById("v1")
     var valorEmRealDolar = "Valor em R$ " + valorEmRealDolar
     valorElemento.innerHTML = valorEmRealDolar
 }
@@ -26,4 +28,19 @@ function ConverterDolar() {
     valorElemento.innerHTML = valorEmRealEuro
     
   }
+
+  function ConverterBit() {
+    var valorElemento = document.getElementById("valor3")
+    var valor3 = valorElemento.value
+    var valorNumber3 = parseFloat(valor3)
+
+var valorEmRealBit = valorNumber3.toFixed(2) * 40.92
+console.log(valorEmRealBit) 
+
+var valorElemento = document.getElementById("valorConvertidoBit")
+var valorEmRealBit = "Valor em R$ " + valorEmRealBit
+valorElemento.innerHTML = valorEmRealBit
+
+}
+
   
