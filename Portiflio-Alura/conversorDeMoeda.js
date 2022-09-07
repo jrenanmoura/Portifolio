6cbcffe69aa6f7292cbea704ec07fb4039541a92
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-09-07 16:24:10 -0300

    modified

--- Portiflio-Alura/conversorDeMoeda.js
+++ Portiflio-Alura/conversorDeMoeda.js
@@ -1,13 +1,29 @@
-function Converter() {
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    var valorDolarNumber = parseFloat(valor);
+function ConverterDolar() {
+    var valorElemento = document.getElementById("valor")
+    var valor = valorElemento.value
+    var valorNumber = parseFloat(valor)
   
-    var valorEmReal = valorDolarNumber * 5;
-    console.log(valorEmReal);
+    var valorEmRealDolar = valorNumber * 5
+    console.log(valorEmRealDolar)
+
+    
   
-    var convertido = document.getElementById("valorConvertido");
-    var valorEmReal = "Valor em R$ " + valorEmReal;
-    convertido.innerHTML = valorEmReal;
+    var valorElemento = document.getElementById("valorConvertidoReal")
+    var valorEmRealDolar = "Valor em R$ " + valorEmRealDolar
+    valorElemento.innerHTML = valorEmRealDolar
+}
+
+    function ConverterEuro() {
+        var valorElemento = document.getElementById("valor2")
+        var valor = valorElemento.value
+        var valorNumber2 = parseFloat(valor)
+
+    var valorEmRealEuro = valorNumber2 * 7
+    console.log(valorEmRealEuro) 
+
+    var valorElemento = document.getElementById("valorConvertidoEuro")
+    var valorEmRealEuro = "Valor em R$ " + valorEmRealEuro
+    valorElemento.innerHTML = valorEmRealEuro
+    
   }
   
