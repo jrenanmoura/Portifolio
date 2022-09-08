3f9267cc60e1a2e1d07cc64f3b246e4ce5fb1bbd
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-09-08 02:03:26 -0300

    app concluido

--- Portiflio-Alura/Aplicação de chute numero/app.js
+++ Portiflio-Alura/Aplicação de chute numero/app.js
@@ -1,5 +1,17 @@
+var numero = parseInt(Math.random() * 11);
 
-function Chute(){
-    var c1 = parseInt(document.getElementById(Chutar()).value)
-    console.log(c1)
+
+
+function Chutar() {
+  var chute = parseInt(document.getElementById("valor").value);
+  var elementoResultado = document.getElementById("resultado");
+
+  console.log(chute);
+    if (chute == numero) {
+    elementoResultado.innerHTML = "Acertou Parabéns!!!! ";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Digite um número de 0 a 10!!";
+  } else {
+    elementoResultado.innerHTML = "Tente novamente o numero era " + numero;
+  }
 }
