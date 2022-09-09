784d8754eca9fa6a9989ab87fc007eb63500c49d
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-09-08 23:07:02 -0300

    Criação do MovieFlix

---
+++ Portiflio-Alura/movieflix/movie.js
@@ -0,0 +1,8 @@
+var lista1 = ["https://img1.ak.crunchyroll.com/i/spire2/ab001b1cb053ec0d7b1b47b0f7bf98e71647329181_full.jpg,https://img1.ak.crunchyroll.com/i/spire2/f9fb623496136ec595edb8476df6915d1647329350_main.jpg,https://br.web.img3.acsta.net/pictures/19/09/18/13/46/0198270.jpg,https://img1.ak.crunchyroll.com/i/spire1/be611b65eb36149005789b740efc7a361333664528_main.jpg"]
+
+
+
+for (var indi = 0; indi < lista1.length; indi++ ){
+    document.write("<img src"= + lista1[indi] + ">")
+}
+ 
